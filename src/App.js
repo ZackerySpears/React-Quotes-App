364b8666cf42319e23c37430cd5121286dd4a142
zackerymarkusrayspears <zackery.spears97@gmail.com>
2019-11-04 13:48:35 -0600

    Added in the list of quotes. Set up the QuoteBox Comonent to use quote props passed in. Added functionality to genorate and show a new random quote

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import React, {Component} from 'react';
 import './App.css';
-import './QuoteBox';
 import QuoteBox from './QuoteBox';
+import quotes from './quotes';
 
 class App extends Component {
+  //Setting up the constructor function so we can set our default state
+  constructor(props) {
+    super(props);
+    //Set up the first quote and author as our default state
+    this.state = {
+      quote: quotes[0].quote, author:quotes[0].author
+    }
+  }
+
+  //Create a Function called getRandomQuote that returns a random quote from the array of quotes. Hint: you 'll need to generate a random number between 0 and 1 less than of the array 
+
+  getRandomQuote() {
+    const randomQuoteIndex = Math.floor(Math.random() * quotes.length);
+    return quotes[randomQuoteIndex];
+  } 
+
+  //Function that will be called when we quote button on the QuoteBox component is pressed
+
+  handleClick = () => {
+    //Generate a random quote 
+    const randomQuote = this.getRandomQuote();
+    //Update our state with information from that random quote
+    this.setState({
+      quote: randomQuote.quote, author: randomQuote.author
+    })
+  }
+
   render() {
     return (
       <div>
-        <QuoteBox />
+        <QuoteBox 
+        quote={this.state.quote}
+        author={this.state.author}
+        handleClick={this.handleClick}
+         />
       </div>
     );
   }
--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
+import quotes from "./quotes"; 
 
 class QuoteBox extends Component {
     render() {
+       
         return (
             <div>
-                
+                <h1>{this.props.quote} </h1>
+                <h1>{this.props.author}</h1>
+                <button onClick={this.props.handleClick}>New Quote</button>
             </div>
         );
     }
