1d7e27551a686dcbb662f49bc427c51c1ad39847
zackerymarkusrayspears <zackery.spears97@gmail.com>
2019-11-04 12:42:11 -0600

    Removed boilerplate content from App.js. Createdthe QuoteBox component and loaded a QuoteBox in App.js

---
+++ src/QuoteBox.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class QuoteBox extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default QuoteBox;
